56bab4a4210b446d6bb9e7c03475b8cccf1e5dd4
Arthur <arthur.taragola@student.howest.be>
2020-01-09 15:02:46 +0100

    Get voor vragen

---
+++ backend/GetVragen.cs
@@ -0,0 +1,63 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using MoveForFortune.Models;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Net.Http;
+using System.Diagnostics;
+
+namespace MoveForFortune
+{
+    public static class GetVragen
+    {
+        [FunctionName("GetVragen")]
+        public static async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "v1/vragen")] HttpRequest req, ILogger log)
+        {
+            string connectionString = Environment.GetEnvironmentVariable("ServerConnectionString");
+            List<Vraag> VraagList = new List<Vraag>();
+            log.LogInformation("C# HTTP trigger function processed a request.");
+            try
+            {
+
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    await connection.OpenAsync();
+                    using (SqlCommand sqlCommand = new SqlCommand())
+                    {
+                        sqlCommand.Connection = connection;
+                        sqlCommand.CommandText = "SELECT * FROM Vragen"; //SQL command
+                        SqlDataReader reader = await sqlCommand.ExecuteReaderAsync();
+
+                        // while not always needed
+                        while (await reader.ReadAsync())
+                        {
+                            VraagList.Add(new Vraag()
+                            {
+                                VraagId = int.Parse(reader["VraagId"].ToString()),
+                                Vraagstelling = reader["Vraagstelling"].ToString(),
+                                JuistAntwoord = reader["JuistAntwoord"].ToString(),
+                                Foutantwoord1 = reader["Foutantwoord1"].ToString(),
+                                Foutantwoord2 = reader["Foutantwoord2"].ToString(),
+                                Niveau = int.Parse(reader["Niveau"].ToString()),
+                                ThemaId = int.Parse(reader["ThemaId"].ToString())
+                            });
+                        }
+                    }
+                }
+                return new OkObjectResult(VraagList);
+            }
+            catch (Exception x)
+            {
+                Debug.WriteLine(x);
+                return new StatusCodeResult(500);
+            }
+        }
+    }
+}
--- backend/Models/Vraag.cs
+++ backend/Models/Vraag.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -7,7 +8,9 @@ namespace MoveForFortune.Models
     class Vraag
     {
         public int VraagId { get; set; }
-        public string VraagStelling { get; set; }
+
+        //[JsonProperty(PropertyName = "Vraag")]
+        public string Vraagstelling { get; set; }
         public string JuistAntwoord { get; set; }
         public string Foutantwoord1 { get; set; }
         public string Foutantwoord2 { get; set; }

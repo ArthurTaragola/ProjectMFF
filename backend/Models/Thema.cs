b6afc2e473808ad6e180e5be89c05299e3ffdb44
Arthur <arthur.taragola@student.howest.be>
2020-01-09 10:55:36 +0100

    Copied backend

---
+++ backend/Models/Thema.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoveForFortune.Models
+{
+    public class Thema
+    {
+        public int ThemaId { get; set; }
+        public string Naam { get; set; }
+        public int VraagId { get; set; }
+    }
+}
---
+++ backend/Models/Vraag.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoveForFortune.Models
+{
+    class Vraag
+    {
+        public int VraagId { get; set; }
+        public string VraagStelling { get; set; }
+        public string JuistAntwoord { get; set; }
+        public string Foutantwoord1 { get; set; }
+        public string Foutantwoord2 { get; set; }
+        public int Niveau { get; set; }
+        public int ThemaId { get; set; }
+    }
+}

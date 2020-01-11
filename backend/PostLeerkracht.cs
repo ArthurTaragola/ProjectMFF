1501c460b662f6fb6e647faf12026e17dc1e2d51
Arthur <arthur.taragola@student.howest.be>
2020-01-11 19:29:39 +0100

    Post voor leerkrachten werkt

--- backend/PostLeerkracht.cs
+++ backend/PostLeerkracht.cs
@@ -10,44 +10,48 @@ using Newtonsoft.Json;
 using MoveForFortune.Models;
 using Microsoft.WindowsAzure.Storage;
 using Microsoft.WindowsAzure.Storage.Table;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.Net.Http;
 
 namespace MoveForFortune
 {
     public static class PostLeerkracht
     {
-    //    [FunctionName("PostLeerkracht")]
-    //    public static async Task<IActionResult> Run(
-    //        [HttpTrigger(AuthorizationLevel.Function, "post", Route = "v2/leerkrachten")] HttpRequest req,
-    //        ILogger log)
-    //    {
-    //        try
-    //        {
-    //            string connectionString = Environment.GetEnvironmentVariable("ServerConnectionString");
-    //            string json = await new StreamReader(req.Body).ReadToEndAsync();
-    //            Leerkracht newLeerkracht = JsonConvert.DeserializeObject<Leerkracht>(json);
-    //            //newLeerkracht.RegistrationId = Guid.NewGuid().ToString();//guid genereert een uniek id
-
-    //            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(connectionString);
-    //            CloudTableClient tableClient = storageAccount.CreateCloudTableClient();
-    //            CloudTable table = tableClient.GetTableReference("Leerkracht");
-
-    //            Leerkracht leerkracht = new Leerkracht()
-    //            {
-    //                Voornaam = newLeerkracht.Voornaam,
-    //                Naam = newLeerkracht.Naam,
-    //                Email = newLeerkracht.Email,
-    //                Wachtwoord = newLeerkracht.Wachtwoord
-    //            };
-
-    //            TableOperation insertOperation = TableOperation.Insert(leerk racht);
-    //            await table.ExecuteAsync(insertOperation);
-    //            return new OkObjectResult(newLeerkracht);
-    //        }
-    //        catch (Exception ex)
-    //        {
-    //            log.LogError(ex, "AddRegistrationV2");
-    //            return new StatusCodeResult(500);
-    //        }
-    //    }
+        [FunctionName("PostLeerkracht")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "post", Route = "v2/leerkrachten")] HttpRequest req,
+            ILogger log)
+        {
+            try
+            {
+                string connectionString = Environment.GetEnvironmentVariable("ServerConnectionString");
+                string json = await new StreamReader(req.Body).ReadToEndAsync();
+                Leerkracht leerkracht = JsonConvert.DeserializeObject<Leerkracht>(json);
+                using (SqlConnection con = new SqlConnection())
+                {
+                    con.ConnectionString = connectionString;
+                    await con.OpenAsync();
+                    using (SqlCommand cmd = new SqlCommand())
+                    {
+                        cmd.Connection = con;
+                        cmd.CommandText = "Insert into Leerkracht values (@Voornaam, @Naam,  @Email, @Wachtwoord)";
+                        cmd.Parameters.AddWithValue("@Voornaam", leerkracht.Voornaam);
+                        cmd.Parameters.AddWithValue("@Naam", leerkracht.Naam);
+                        cmd.Parameters.AddWithValue("@Email", leerkracht.Email);
+                        cmd.Parameters.AddWithValue("@Wachtwoord", leerkracht.Wachtwoord);
+                        await cmd.ExecuteNonQueryAsync();
+                    }
+                }
+                return new StatusCodeResult(200);
+            }
+            catch(Exception ex)
+            {
+                log.LogError(ex + "     ---->AddRegistration");
+                return new StatusCodeResult(500);
+            }
+        }
     }
 }

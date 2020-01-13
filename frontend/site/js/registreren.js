8a5e903b6505bbd33664062e94c7af4a5a27e116
Jonas <jonas.verhenne@student.howest.be>
2020-01-13 18:23:55 +0100

    post leerkracht (registreren)
    javascript van leerkracht registreren afgewerkt

---
+++ frontend/site/js/registreren.js
@@ -0,0 +1,33 @@
+function DoSubmit()
+{
+    let xhr = new XMLHttpRequest();
+
+    let firstName = document.getElementById("voornaam").value;
+    let name = document.getElementById("naam").value;
+    let email = document.getElementById("email").value;
+    let password = document.getElementById("ww").value;
+
+    xhr.open("POST", "https://moveforfortunefunction.azurewebsites.net/api/v2/leerkrachten");
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.send(JSON.stringify({
+        "Voornaam": firstName,
+        "Naam": name,
+        "Email": email,
+        "wachtwoord": password
+    }));
+    xhr.onreadystatechange = function ()
+    {
+        if(xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200)
+        {
+            console.log("the account has succesfully been made");
+            //go to new page
+        }
+    }
+}
+
+const init = function()
+{
+    console.log("DOM loaded");
+};
+
+document.addEventListener('DOMContentLoaded', init);

cd31ecdb663a14f0b08457553eeca4fd34f3ddd4
Arthur <arthur.taragola@student.howest.be>
2020-01-20 15:52:38 +0100

    gitflow-feature-stash: bugfix
    bug is fixed

--- frontend/site/js/vragenbekijken.js
+++ frontend/site/js/vragenbekijken.js
@@ -193,7 +193,8 @@ const getAPI = async function(niveau, thema)
   questionList = [];
     try
     {
-        const data = await fetchData(`https://moveforfortunefunction.azurewebsites.net/api/v1/vragen/${niveau}/${thema}`);
+      console.log(niveau, thema)
+        const data = await fetchData(`https://moveforfortunefunction.azurewebsites.net/api/v1/vragen/${thema}/${niveau}`);
         for (let k = 0; k < data.length; k++)
         {
             questionList.push(data[k]);
@@ -236,44 +237,45 @@ const FillInData = function()
         </td> 
     </tr>`;
 
-    for (let i = 1; i < questionList.length; i++)
-    {
-        let vraag = questionList[i].vraagstelling;
-        let A=  questionList[i].juistAntwoord;
-        let B=  questionList[i].foutAntwoord1;
-        let C=  questionList[i].foutAntwoord2;
-        console.log(vraag);
-        console.log(A);
-        console.log(B);
-        console.log(C);
+      for (let i = 1; i < questionList.length; i++)
+      {
+          let vraag = questionList[i].vraagstelling;
+          let A=  questionList[i].juistAntwoord;
+          let B=  questionList[i].foutAntwoord1;
+          let C=  questionList[i].foutAntwoord2;
+          console.log(vraag);
+          console.log(A);
+          console.log(B);
+          console.log(C);
 
-        htmlQuestion +=
-        `<tr id="js-question" class="c-table-color"></tr>
-        <td>${vraag}</td>
-        <td>A.</td> <td>${A}</td>
-        <td>B.</td> <td>${B}</td>
-        <td>C.</td> <td>${C}</td>
-        <td>
-            <div class="popup" onclick="myFunction()"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#08518B" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"/><path d="M12 8c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2zm0 2c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm0 6c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2z"/></svg>
-                <span class="popuptext" id="myPopup">
-                    <table class="c-center">                                    
-                            <button class="c-button__popup" >
-                                <svg style="margin-bottom: -4px; margin-right:8px;" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#08518B" viewBox="0 0 24 24"><path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.39-.39-1.02-.39-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z"/><path d="M0 0h24v24H0z" fill="none"/></svg>
-                                Aanpassen  
-                            </button>
-                            <button class="c-button__popup">
-                                <svg style="margin-bottom: -4px; margin-right:8px;" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#08518B" viewBox="0 0 24 24"><path fill="none" d="M0 0h24v24H0V0z"/><path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zm2.46-7.12l1.41-1.41L12 12.59l2.12-2.12 1.41 1.41L13.41 14l2.12 2.12-1.41 1.41L12 15.41l-2.12 2.12-1.41-1.41L10.59 14l-2.13-2.12zM15.5 4l-1-1h-5l-1 1H5v2h14V4z"/><path fill="none" d="M0 0h24v24H0z"/></svg>
-                                Verwijderen 
-                            </button>
-                        
-                    </table>
-                </span>
-              </div>
-        </td> 
-      </tr>`;
+          htmlQuestion +=
+          `<tr id="js-question" class="c-table-color"></tr>
+          <td>${vraag}</td>
+          <td>A.</td> <td>${A}</td>
+          <td>B.</td> <td>${B}</td>
+          <td>C.</td> <td>${C}</td>
+          <td>
+              <div class="popup" onclick="myFunction()"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#08518B" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"/><path d="M12 8c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2zm0 2c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm0 6c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2z"/></svg>
+                  <span class="popuptext" id="myPopup">
+                      <table class="c-center">                                    
+                              <button class="c-button__popup" >
+                                  <svg style="margin-bottom: -4px; margin-right:8px;" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#08518B" viewBox="0 0 24 24"><path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.39-.39-1.02-.39-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z"/><path d="M0 0h24v24H0z" fill="none"/></svg>
+                                  Aanpassen  
+                              </button>
+                              <button class="c-button__popup">
+                                  <svg style="margin-bottom: -4px; margin-right:8px;" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#08518B" viewBox="0 0 24 24"><path fill="none" d="M0 0h24v24H0V0z"/><path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zm2.46-7.12l1.41-1.41L12 12.59l2.12-2.12 1.41 1.41L13.41 14l2.12 2.12-1.41 1.41L12 15.41l-2.12 2.12-1.41-1.41L10.59 14l-2.13-2.12zM15.5 4l-1-1h-5l-1 1H5v2h14V4z"/><path fill="none" d="M0 0h24v24H0z"/></svg>
+                                  Verwijderen 
+                              </button>
+                          
+                      </table>
+                  </span>
+                </div>
+          </td> 
+        </tr>`;
 
-        document.getElementById("js-question").innerHTML = htmlQuestion;
-    }
+      }
+    
+    document.getElementById("js-question").innerHTML = htmlQuestion;
 
 
 

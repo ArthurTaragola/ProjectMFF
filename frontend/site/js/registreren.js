b69adaf2e1060c7e93daf640c3c34889c18313fe
Jonas <jonas.verhenne@student.howest.be>
2020-01-21 11:20:07 +0100

    fixed some things
    kan niet meer registreren als knop grijs is
    superuser kan enkel maar quiz starten
    bij thema is de knop in het begin nu ook grijs
    vragen zijn nu ook afh van geselecteerd niveau

--- frontend/site/js/registreren.js
+++ frontend/site/js/registreren.js
@@ -113,29 +113,32 @@ const yellowButton = function ()
 
 function DoSubmit()
 {
-    let xhr = new XMLHttpRequest();
-
-    let firstName = document.getElementById("voornaam").value;
-    let name = document.getElementById("naam").value;
-    let email = document.getElementById("email").value;
-    let password = document.getElementById("ww").value;
-
-    xhr.open("POST", "https://moveforfortunefunction.azurewebsites.net/api/v2/leerkrachten");
-    xhr.setRequestHeader('Content-Type', 'application/json');
-    xhr.send(JSON.stringify({
-        "Voornaam": firstName,
-        "Naam": name,
-        "Email": email,
-        "wachtwoord": password
-    }));
-    xhr.onreadystatechange = function ()
+    if (alleventListenersValid)
     {
-        if(xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200)
+        let xhr = new XMLHttpRequest();
+
+        let firstName = document.getElementById("voornaam").value;
+        let name = document.getElementById("naam").value;
+        let email = document.getElementById("email").value;
+        let password = document.getElementById("ww").value;
+
+        xhr.open("POST", "https://moveforfortunefunction.azurewebsites.net/api/v2/leerkrachten");
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        xhr.send(JSON.stringify({
+            "Voornaam": firstName,
+            "Naam": name,
+            "Email": email,
+            "wachtwoord": password
+        }));
+        xhr.onreadystatechange = function ()
         {
-            console.log("the account has succesfully been made");
-            response = xhr.responseText; 
-            console.log(response); //expect leerkrachtId
-            //go to new page
+            if(xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200)
+            {
+                console.log("the account has succesfully been made");
+                //response = xhr.responseText; 
+                //console.log(response); //expect leerkrachtId
+                window.location.href = "login.html";
+            }
         }
     }
 }

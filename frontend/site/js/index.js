7ef80b8a3f9db98d2c6bb13f3d76b016a1618ccb
Arthur <arthur.taragola@student.howest.be>
2020-01-28 15:53:58 +0100

    gitflow-feature-stash: information_button

--- frontend/site/js/index.js
+++ frontend/site/js/index.js
@@ -14,6 +14,28 @@ const goToRegisterPage = function ()
     window.location.href = "registreren.html";
 }
 
+const popup = function()
+{
+    console.log("okioki")
+    Swal.fire({
+        title: '<strong>Information</strong>',
+        icon: 'info',
+        html:
+          'bekijk de werking van get spel in volgende ' +
+          '<a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">instructie video</a> ' +
+          'vogels <b>icons</b> can be found <a href="https://www.flaticon.com/packs/bird-collection">here.</a><br>' + 
+          '<b>Soundeffects</b> can be found <a href="https://freesound.org/home/">here</a>',
+        showCloseButton: true,
+        showCancelButton: false,
+        focusConfirm: false,
+        confirmButtonText:
+          '<i class="fa fa-thumbs-up"></i> Geweldig!',
+        cancelButtonText:
+          '<i class="fa fa-thumbs-down"></i>',
+        cancelButtonAriaLabel: 'Thumbs down'
+      })
+}
+
 const init = function()
 {
     console.log("DOM loaded");
@@ -23,6 +45,8 @@ const init = function()
     defaultUserButton.addEventListener('click', goToQuizPage);
     registerButton = document.querySelector('.js-registerButton');
     registerButton.addEventListener('click', goToRegisterPage);
+    info = document.querySelector('#js-info')
+    info.addEventListener("click", popup);
 }
 
 document.addEventListener('DOMContentLoaded', init);
